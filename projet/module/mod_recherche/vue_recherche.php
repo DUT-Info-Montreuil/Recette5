923fceea793228b32d633db85adfd509949b8af5
Priyank Solanki <psolanki@condorcet93.fr>
2022-10-26 12:46:37 +0200

    modifcation css recette

--- projet/module/mod_recherche/vue_recherche.php
+++ projet/module/mod_recherche/vue_recherche.php
@@ -11,9 +11,15 @@ class VueRecherche extends VueGenerique{
         }else{
             foreach($tabR as $value){
                 echo 
-                '<div class="recette">
-                    <h1>'.$value['titre'].'</h1>
-                    '.$value['description'].'
+
+                '<div id="recette">
+                    <div class="photo">
+
+                    </div>
+                    <div class="info">
+                        <h1>'.$value['titre'].'</h1>
+                        <p>'.$value['description'].'</p>
+                    </div>
                 </div>';
             }
         }
