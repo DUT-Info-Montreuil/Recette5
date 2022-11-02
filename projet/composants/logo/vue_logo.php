dc6deeb14f155180d6dad3d1e6f9025f9e0c2531
PriyankSolanki <psolanki@condorcet93.fr>
2022-11-02 11:21:14 +0100

    rectification du merge

--- projet/composants/logo/vue_logo.php
+++ projet/composants/logo/vue_logo.php
@@ -7,7 +7,7 @@ class VueLogo{
     }
 
     public function logo(){
-        $this->logo = '<a href="index.php?module=connexion&action=bienvenue"><img id="logo" alt="logo du site" src="image/5.png"></a>';
+        $this->logo = '<a href="index.php?module=connexion&action=bienvenue"><img id="logo" alt="logo du site" src="image/logo.png"></a>';
         return $this->logo;
     }
 
--- projet/module/mod_recette/vue_recette.php
+++ projet/module/mod_recette/vue_recette.php
@@ -8,11 +8,9 @@
              
          }
        
-         public function afficherMesRecette($recette){
-       
+         public function afficherMesRecette($recette){    
             foreach( $recette as $value ){
                echo 
-
                '<a href="index.php?module=recette&action=afficherMaRecette&idRecette='.$value['idRecette'].'"><div id="recette"><div class="photo">
                <img id="pho" alt="photo de la recette" src="image_recette/'.$value['photo'].'" >
                    </div>
@@ -26,20 +24,15 @@
 
 
          public function afficherMaRecette($recette){
-       
             echo '<h1> '.$recette['titre'].'</h1>';   
-            echo' description  : <br>'.$recette['description'].' <br>';
-            
-            
+            echo' description  : <br>'.$recette['description'].' <br>';     
          }
 
          public function afficherPhoto($photo){
-           if(!isset($photo['photo'])){
+         if(!isset($photo['photo']))
             echo '<img src="image/index.jpeg" width="200" height="200">';
-           }else{
-            echo '<img src="image_recette/'.$photo['photo'].'" width="200" height="200">';
-           }
-            
+         else
+            echo '<img src="image_recette/'.$photo['photo'].'" width="200" height="200">';          
          }
 
          public function afficherIngredientDeMaRecette($Ingredient){
@@ -49,64 +42,55 @@
             }
           }
 
-          public function afficherNbLikes($nbLike){
+         public function afficherNbLikes($nbLike){
             echo "nb de likes : ".$nbLike['count(aime)']; 
-
-          }
-
-         public function afficherChoixNbIngredient(){
-            echo '</br>Ajouter une nouvelle Recette
-       
-             <form method="post" action="index.php?module=recette&action=AfficherFormAjoutRecette">
-             Entrez le nombre d ingredient: <input type="text" name="nbIngr">
-            <input type="submit" value="envoyer"> </form>';
-                     
-             
          }
 
-        public function afficher_form_Recette($tabIngr,$nbIngr){
-            
-            echo '</br>Ajouter une nouvelle Recette<br>';
-          
-            echo'  <form method="post" action="index.php?module=recette&action=ajouterRecetteDansLaBD&nbIngr='.$nbIngr.'" enctype="multipart/form-data">
+         public function afficherChoixNbIngredient(){
+            echo '
+            <form method="post" action="index.php?module=recette&action=AfficherFormAjoutRecette">
+               Entrez le nombre d\'ingredient: <input type="text" name="nbIngr">
+               <input type="submit" value="envoyer">
+            </form>';   
+         }
+
+        public function afficher_form_Recette($tabIngr,$nbIngr){   
+            echo'
+            <form method="post" action="index.php?module=recette&action=ajouterRecetteDansLaBD&nbIngr='.$nbIngr.'" enctype="multipart/form-data">
                <label for="file">Fichier</label>
                <input type="file" name="file"><br />
                <label for="titre"> nom Recette :</label>
                <input type="text" name="titre">
-                <br />
-                <label for="tpsPreparrration">temps de préparation : </label>
-                <input type="text" name="tpsPreparration">
-                <br />
-                Description: <input type="textArea" name="description">
-                <br />
-
-                note annexe: <input type="textArea" name="annexe">
-                <br />
-                vegan: <input type="checkbox" name="vegan">
-                <br />'
-            ;
-            for ($i=0; $i<$nbIngr; $i++) {
-                echo'<br>';
-                echo' Ingredient '.($i+1).'  <select name="ingredient'.$i.'">';
-                foreach( $tabIngr as $value ){
-                echo'   <option value="'.$value['idIngredient'].' ">'.$value['nomIngredient'].'</option>';
-                }     
-               echo' </select>';
-               echo'  quantite : <input type="text" name="quantite'.$i.'">';   
-               echo'<select name="unite'.$i.'">';   
-               echo'<option value="kg">kg</option>';
-               echo'<option value="g">g</option>';
-               echo'<option value="mg">mg</option>';
-               echo'<option value="nb">nb</option>';
-               echo'<option value="l">l</option>';
-               echo'<option value="ml">ml</option>';
-               echo'</select><br><br>';
-               echo'<br>';
-               
-          }
-
-        echo ' <input type="submit" value="envoyer"> </form>';
-         
+               <br />
+               <label for="tpsPreparrration">temps de préparation : </label>
+               <input type="text" name="tpsPreparration">
+               <br />
+               Description: <input type="textArea" name="description">
+               <br />
+               note annexe: <input type="textArea" name="annexe">
+               <br />
+               vegan: <input type="checkbox" name="vegan">
+               <br />' ;
+               for ($i=0; $i<$nbIngr; $i++) {
+                  echo'<br>';
+                  echo' Ingredient '.($i+1).'  <select name="ingredient'.$i.'">';
+                  foreach( $tabIngr as $value ){
+                      echo'   <option value="'.$value['idIngredient'].' ">'.$value['nomIngredient'].'</option>';
+                  }     
+                  echo'</select>';
+                  echo'quantite : <input type="text" name="quantite'.$i.'">';   
+                  echo'<select name="unite'.$i.'">';   
+                  echo'<option value="kg">kg</option>';
+                  echo'<option value="g">g</option>';
+                  echo'<option value="mg">mg</option>';
+                  echo'<option value="nb">nb</option>';
+                  echo'<option value="l">l</option>';
+                  echo'<option value="ml">ml</option>';
+                  echo'</select><br><br>';
+                  echo'<br>';  
+               }
+            echo'<input type="submit" value="envoyer"> </form>';
         }
+        
     }
 ?>
