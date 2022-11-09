682fef7986c39aa696e917c498179eb7a3a24f1c
ecyriaque <101125367+ecyriaque@users.noreply.github.com>
2022-11-09 10:05:24 +0100

    ajout de htmlspecialcars a chaque affichage dans le module Recette afin de contrer les failles XSS

--- projet/module/mod_recette/vue_recette.php
+++ projet/module/mod_recette/vue_recette.php
@@ -11,12 +11,12 @@
          public function afficherMesRecette($recette){    
             foreach( $recette as $value ){
                echo 
-               '<a href="index.php?module=recette&action=afficherMaRecette&idRecette='.$value['idRecette'].'"><div id="recette"><div class="photo">
-               <img id="pho" alt="photo de la recette" src="image_recette/'.$value['photo'].'" >
+               '<a href="index.php?module=recette&action=afficherMaRecette&idRecette='.htmlspecialchars($value['idRecette']).'"><div id="recette"><div class="photo">
+               <img id="pho" alt="photo de la recette" src="image_recette/'.htmlspecialchars($value['photo']).'" >
                    </div>
                    <div class="info">
-                       <h1>'.$value['titre'].'</h1>
-                       <p>'.$value['description'].'</p>
+                       <h1>'.htmlspecialchars($value['titre']).'</h1>
+                       <p>'.htmlspecialchars($value['description']).'</p>
                    </div>
                </div></a>';
             }
@@ -24,28 +24,28 @@
 
 
          public function afficherMaRecette($recette){
-            echo '<h1> '.$recette['titre'].'</h1>';   
-            echo' description  : <br>'.$recette['description'].' <br>';     
+            echo '<h1> '.htmlspecialchars($recette['titre']).'</h1>';   
+            echo' description  : <br>'.htmlspecialchars($recette['description']).' <br>';     
             if($_SESSION['id']==$recette['idUtilisateur'])
-            echo '<a href="index.php?module=recette&action=AffichermodifierMaRecette&idRecette='.$recette['idRecette'].'">modifier recette </a>';
+            echo '<a href="index.php?module=recette&action=AffichermodifierMaRecette&idRecette='.htmlspecialchars($recette['idRecette']).'">modifier recette </a>';
          }
 
          public function afficherPhoto($photo){
             if(!isset($photo['photo']))
                echo '<img src="image/index.jpeg" width="200" height="200">';
             else
-               echo '<img src="image_recette/'.$photo['photo'].'" width="200" height="200">';          
+               echo '<img src="image_recette/'.htmlspecialchars($photo['photo']).'" width="200" height="200">';          
             }
 
          public function afficherIngredientDeMaRecette($Ingredient){
             echo '<h2>voici la liste des ingrédients</h2>';
             foreach( $Ingredient as $value ){
-               echo '<br> '.$value['nomIngredient'].' : '.$value['Quantite'].' '.$value['unite'].'';
+               echo '<br> '.htmlspecialchars($value['nomIngredient']).' : '.htmlspecialchars($value['Quantite']).' '.htmlspecialchars($value['unite']).'';
             }
           }
 
          public function afficherNbLikes($nbLike){
-            echo "nb de likes : ".$nbLike['count(aime)']; 
+            echo "nb de likes : ".htmlspecialchars($nbLike['count(aime)']); 
             }
 
          public function afficherChoixNbIngredient(){
@@ -57,7 +57,7 @@
 
          public function afficher_form_Recette($tabIngr,$nbIngr){   
             echo'
-            <form method="post" action="index.php?module=recette&action=ajouterRecetteDansLaBD&nbIngr='.$nbIngr.'" enctype="multipart/form-data">
+            <form method="post" action="index.php?module=recette&action=ajouterRecetteDansLaBD&nbIngr='.htmlspecialchars($nbIngr).'" enctype="multipart/form-data">
                <label for="file">Fichier</label>
                <input type="file" name="file"><br />
                <label for="titre"> nom Recette :</label>
@@ -76,7 +76,7 @@
                   echo'<br>';
                   echo' Ingredient '.($i+1).'  <select name="ingredient'.$i.'">';
                   foreach( $tabIngr as $value ){
-                      echo'   <option value="'.$value['idIngredient'].' ">'.$value['nomIngredient'].'</option>';
+                      echo'   <option value="'.htmlspecialchars($value['idIngredient']).' ">'.htmlspecialchars($value['nomIngredient']).'</option>';
                   }     
                   echo'</select>';
                   echo'quantite : <input type="text" name="quantite'.$i.'">';   
@@ -97,18 +97,18 @@
     
     
          echo'
-         <form method="post" action="index.php?module=recette&action=modifierMaRecette&idRecette='.$recette['idRecette'].'" enctype="multipart/form-data">
+         <form method="post" action="index.php?module=recette&action=modifierMaRecette&idRecette='.htmlspecialchars($recette['idRecette']).'" enctype="multipart/form-data">
             <label for="file">photo</label>
             <input type="file" name="file"><br />
             <label for="titre"> nom Recette :</label>
-            <input type="text" name="titre" value="'.$recette['titre'].'">
+            <input type="text" name="titre" value="'.htmlspecialchars($recette['titre']).'">
             <br />
             <label for="tpsPreparrration">temps de préparation : </label>
-            <input type="text" name="tpsPreparration" value="'.$recette['tpsPreparration'].'">
+            <input type="text" name="tpsPreparration" value="'.htmlspecialchars($recette['tpsPreparration']).'">
             <br />
-            Description: <input type="textArea" name="description" value="'.$recette['description'].'">
+            Description: <input type="textArea" name="description" value="'.htmlspecialchars($recette['description']).'">
             <br />
-            note annexe: <input type="textArea" name="annexe" value="'.$recette['noteAnnexe'].'">
+            note annexe: <input type="textArea" name="annexe" value="'.htmlspecialchars($recette['noteAnnexe']).'">
             <br />' ;
 
             if($recette['vegan']==1){
