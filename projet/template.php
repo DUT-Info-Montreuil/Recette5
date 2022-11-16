66cdbbae541675ad7089b7615b52d60db7bab38e
ecyriaque <101125367+ecyriaque@users.noreply.github.com>
2022-11-16 14:37:47 +0100

    Update template.php

--- projet/template.php
+++ projet/template.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 
 <html lang="fr">
-	<head>
-		<meta charset="UTF-8">
+<head>
+		<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
 		<title>Recette5</title>
         <link rel="stylesheet" href="css.css"/>
 	</head>
