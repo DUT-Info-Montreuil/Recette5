ffc15a12df87f02c6f1dc95610e9518f3b2550c1
PriyankSolanki <psolanki@condorcet93.fr>
2022-11-26 03:14:00 +0100

    correction bugs affichage

--- projet/template.php
+++ projet/template.php
@@ -28,17 +28,16 @@
                 echo $affiche;
             ?>
         </main>
-		<footer >
 
-            <div class="container">
-            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 " >
+                <div id="divf" align="center"></div>
                 <p class="col-md-4 mb-0 text-muted">&copy; 2022 Company, Inc</p>
 
                 <a href="index.php?module=connexion&action=bienvenue" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
                 <img src="image/logo.png" width="32" height="32">
                 </a>
 
-                <ul class="nav col-md-4 justify-content-end">
+                <ul class="nav col-md-4 justify-content-end" id="liste">
                 <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
                 <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
                 <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
@@ -46,8 +45,6 @@
                 <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
                 </ul>
             </footer>
-            </div>
-		</footer>
         </div>
 	</body>
 </html>
