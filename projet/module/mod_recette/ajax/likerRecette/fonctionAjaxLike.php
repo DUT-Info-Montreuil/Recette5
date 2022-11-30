712a5c7594c576ea090a8958183ba6aaaa11a0e7
ecyriaque <101125367+ecyriaque@users.noreply.github.com>
2022-11-30 12:38:56 +0100

    Update fonctionAjaxLike.php

--- projet/module/mod_recette/ajax/likerRecette/fonctionAjaxLike.php
+++ projet/module/mod_recette/ajax/likerRecette/fonctionAjaxLike.php
@@ -15,6 +15,11 @@
 
          $(document).ready(function(){
            
+            $("#supprimerRecette").append('<button class="btn btn-lg btn-danger" id="supRec">Supprimer</button>');
+            $("#supprimerRecette").append('<button class="btn btn-lg btn-secondary" class="btn-close" data-bs-dismiss="modal">Annuler</button>');
+
+
+
             $('#divBoutonDeLike').append('<button class="btn btn-lg btn-secondary" id="boutonDeLike"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-heart-fill"    ><path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"></path></svg></span></button>');
             $('#divBoutonDeLike').append('<button type="button" class="btn btn-lg btn-primary" id="boutonDeDisLike"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-heart-fill" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"></path></svg></button>');
             $("#boutonDeLike").hide();
@@ -70,6 +75,20 @@
         );
         }
     );
+    
+    $(
+        function(){
+            $("#supRec").click(
+            function(){
+                
+                $.post("module/mod_recette/ajax/likerLaRecette/supprimerRecette.php",{idRecette:<?php echo$_GET['idRecette']?>},function(data){  
+                    $('#supprimerRecette').empty().append("votre recette s'est bien supprimer");
+                    window.location.href = "index.php?module=recherche&action=toute";
+                });
+            }
 
+        );
+        }
+    );
 
    </script>
