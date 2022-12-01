5c87c1f981543890a03b89400bda9aa67c1daea4
PriyankSolanki <psolanki@condorcet93.fr>
2022-12-01 22:10:38 +0100

    maj categories

--- projet/module/mod_recette/ajax/ajouterCategorie/fonctionCategorie.php
+++ projet/module/mod_recette/ajax/ajouterCategorie/fonctionCategorie.php
@@ -1,6 +1,7 @@
 <script>
 $().ready(function(){  
-    $("#categoriesSelectionne").append('<input class="cat form-check-input mt-0" type="checkbox" name="plat" value="PLAT"> <label class="form-check-label" for="cat">Plat</label></br>');
+    $("#categoriesSelectionne").append('<input class="cat form-check-input mt-0" type="checkbox" name="plat" value="PLAT"> <label class="form-check-label" for="cat">PLAT</label>');
+    $("#categoriesSelectionne").append('<input class="cat form-check-input mt-0" type="checkbox" name="dessert" value="DESSERT"> <label class="form-check-label" for="cat">DESSERT</label>');
 
 });
 
