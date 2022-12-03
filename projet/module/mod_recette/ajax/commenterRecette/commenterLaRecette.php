90724dfe83eb353fa00c4626c33a843ba94e2493
ecyriaque <101125367+ecyriaque@users.noreply.github.com>
2022-12-03 11:05:58 +0100

    correction du bug qui permet de poster un commentaire vide
    Co-Authored-By: PriyankSolanki <101139891+PriyankSolanki@users.noreply.github.com>

--- projet/module/mod_recette/ajax/commenterRecette/commenterLaRecette.php
+++ projet/module/mod_recette/ajax/commenterRecette/commenterLaRecette.php
@@ -3,21 +3,25 @@
 extract($_POST);
  session_start();
 $idUtilisateur=$_SESSION['id'];
-
+if($commentaire==""){
+      echo json_encode("vide");
+}else{
       $bdd=new PDO('mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201631','dutinfopw201631','mudepuna');
    
 
-         $sth=$bdd->prepare("INSERT INTO `commentaire` (`idCommentaire`, `commentaire`, `idUtilisateur`, `idRecette`, `dateAjout`, `heureAjout`) VALUES (NULL, ?,?,?, now(), curtime());");
-         $sth->execute(array($commentaire,$idUtilisateur,$idRecette));
-    
-         $sth = $bdd->prepare("SELECT MAX(idCommentaire) FROM commentaire ");
-         $sth->execute();
-         $row = $sth->fetch();
+      $sth=$bdd->prepare("INSERT INTO `commentaire` (`idCommentaire`, `commentaire`, `idUtilisateur`, `idRecette`, `dateAjout`, `heureAjout`) VALUES (NULL, ?,?,?, now(), curtime());");
+      $sth->execute(array($commentaire,$idUtilisateur,$idRecette));
+ 
+      $sth = $bdd->prepare("SELECT MAX(idCommentaire) FROM commentaire ");
+      $sth->execute();
+      $row = $sth->fetch();
 
-         $sth = $bdd->prepare("SELECT commentaire,photo,login,heureAjout,dateAjout,idCommentaire FROM commentaire NATURAL JOIN Utilisateurs where idCommentaire=? ");
-         $sth->execute(array($row['MAX(idCommentaire)']));
-         $row2 = $sth->fetch();
-         echo json_encode($row2);
+      $sth = $bdd->prepare("SELECT commentaire,photo,login,heureAjout,dateAjout,idCommentaire FROM commentaire NATURAL JOIN Utilisateurs where idCommentaire=? ");
+      $sth->execute(array($row['MAX(idCommentaire)']));
+      $row2 = $sth->fetch();
+      echo json_encode($row2);
+}
+    
    
 
      
