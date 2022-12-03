965318a63ee54170d6a5d2880d3f9c101c26a7fb
ecyriaque <101125367+ecyriaque@users.noreply.github.com>
2022-12-03 18:16:40 +0100

    affichage de tous les comm si barre de recherche vide

--- projet/module/mod_admin/ajax/commentaire/fonctionAjaxCommentaire.php
+++ projet/module/mod_admin/ajax/commentaire/fonctionAjaxCommentaire.php
@@ -36,7 +36,9 @@ $(document).ready(function(){
             $("#chercherCom").keyup(
                 function(){
                     var mot =document.getElementById("chercherCom").value
-                    $.post("module/mod_admin/ajax/commentaire/chercherMotDansCom.php",{mot:mot},function(data){  
+
+                    if(mot==""||mot==null){
+                        $.post("module/mod_admin/ajax/commentaire/chercherToutCom.php",{mot:mot},function(data){  
                         $('#divCommentaire').empty();
                             $(data).each(function(i, commentaire){
                                 $('#divCommentaire').append('<br>');
@@ -44,6 +46,17 @@ $(document).ready(function(){
                         });           
                                   
                       });
+                    }else{
+                        $.post("module/mod_admin/ajax/commentaire/chercherMotDansCom.php",{mot:mot},function(data){  
+                        $('#divCommentaire').empty();
+                            $(data).each(function(i, commentaire){
+                                $('#divCommentaire').append('<br>');
+                                $('#divCommentaire').append('<div id="commentaire'+commentaire.idCommentaire+'" class="media"><a class="pull-left" href="#"><img id="pp" width="100" class="media-object" src="image/image_utilisateur/'+commentaire.photo+'" alt=""></a> <div class="media-body"><h4 class="media-heading">'+commentaire.login+' a écrit le '+commentaire.dateAjout+' a '+commentaire.heureAjout+' :<button type="button"  id="BoutonSupprimerCommentaire" value="'+commentaire.idCommentaire+'" class="btn btn-danger btn_remove">supprimer commentaire</button> </p></h4>id commentaire :'+commentaire.idCommentaire+'<p>texte :'+commentaire.commentaire+'</p></div></div>');
+                        });           
+                                  
+                      });
+                    }
+                   
                
             }
         );
