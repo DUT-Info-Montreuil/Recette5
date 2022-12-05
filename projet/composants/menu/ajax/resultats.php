93908c41b22508f6892d64c1f04544f116b27b19
ecyriaque <101125367+ecyriaque@users.noreply.github.com>
2022-12-05 23:50:47 +0100

    Update resultats.php

--- projet/composants/menu/ajax/resultats.php
+++ projet/composants/menu/ajax/resultats.php
@@ -4,7 +4,7 @@ extract($_POST);
     $bdd=new PDO('mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201631','dutinfopw201631','mudepuna');
 
     $sth=$bdd->prepare("SELECT titre,idRecette,photo FROM Recette natural join photo WHERE titre LIKE ?");
-    $sth->execute(array($titre.'%'));
+    $sth->execute(array('%'.$titre.'%'));
     $row = $sth->fetchAll();
     if($row==null)
         echo json_encode("vide");
